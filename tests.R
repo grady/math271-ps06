09e415ad2590cb18d2ff83433ab712230c1ad067
Grady Weyenberg <gradysw@hawaii.edu>
2022-02-07 22:43:33 -1000

    initial commit of new problem set

--- tests.R
+++ tests.R
@@ -1,2 +1,7 @@
-## execute all code in the solution file
-try(knitr::knit(text=readLines('PSxx_solution.Rmd')))
+test_that("Solution file has expected objects", {
+  # the solution document to knits without errors
+  expect_error(knitr::knit(text=readLines('PS06_solution.Rmd')), NA)
+  # the `bty_score` and `bty_age` objects are linear models
+  expect_s3_class(bty_score, "lm")
+  expect_s3_class(bty_age, "lm")
+})
