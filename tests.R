1b3271f1424f9a061881ccc5741580a84e07a50b
Grady Weyenberg <gradysw@hawaii.edu>
2024-02-11 23:37:54 -1000

    updates to autograding scripts

--- tests.R
+++ tests.R
@@ -1,7 +1,17 @@
-test_that("Solution file has expected objects", {
-  # the solution document to knits without errors
-  expect_error(knitr::knit(text=readLines('PS06_solution.Rmd')), NA)
-  # the `bty_score` and `bty_age` objects are linear models
+test_that("<5>Solution file exists and knits",{
+  expect_true(file.exists("solution.Rmd"))
+  suppressWarnings(expect_error(knitr::knit("solution.Rmd", quiet = TRUE, envir=globalenv()), NA))
+})
+
+test_that("<1> bty_score object exists", {
   expect_s3_class(bty_score, "lm")
+})
+
+test_that("<3> evals_resid object includes score_pred and score_resid columns", {
+  expect_error(expect_vector(evals_resid$score_pred, numeric()), NA)
+  expect_error(expect_vector(evals_resid$score_resid, numeric()), NA)
+})
+
+test_that("<1> bty_age object exists", {
   expect_s3_class(bty_age, "lm")
 })
